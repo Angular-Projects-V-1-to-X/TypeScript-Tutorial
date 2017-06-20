975ef73ad6bf33f06a7bfd8f09903e6116a2ce82
niranjan-reddy <niranjan-reddy@gmx.com>
2017-06-20 14:37:35 +0530

    first commit

---
+++ tstut.js
@@ -0,0 +1,10 @@
+var myName = "Niranjan";
+var myAge = 36;
+var canVote = true;
+var anything = "dog";
+anything = 3;
+document.getElementById("tsStuff")
+    .innerHTML = "My Name is " + myName;
+document.write("canVote is a " + typeof (canVote) + "<br />");
+document.write("myName is a " + typeof (myName) + "<br />");
+document.write("anything is a " + typeof (anything) + "<br />");
